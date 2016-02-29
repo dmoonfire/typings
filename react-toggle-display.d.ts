553783bba1d3a64def3638569bbf7f2192d008eb
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2016-02-29 10:11:42 -0600

    react-toggle-display: Trying to get a better export for typings.

--- react-toggle-display.d.ts
+++ react-toggle-display.d.ts
@@ -1,4 +1,6 @@
 import * as React from "react";
-export default declare class ToggleDisplay extends React.Component<any, any> {
+export declare class ToggleDisplay extends React.Component<any, any> {
     render(): JSX.Element;
 }
+
+export default ToggleDisplay;
