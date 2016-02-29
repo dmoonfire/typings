f1a1c34210eb415856705f476e145077200c9068
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2016-02-28 21:04:01 -0600

    Fixed the default export.

--- react-toggle-display.d.ts
+++ react-toggle-display.d.ts
@@ -1,4 +1,4 @@
 import * as React from "react";
-export declare class ToggleDisplay extends React.Component<any, any> {
+export default declare class ToggleDisplay extends React.Component<any, any> {
     render(): JSX.Element;
 }
