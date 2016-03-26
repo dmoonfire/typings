bd2ea2b720ab61ca5105d9161feae4942fac6bed
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2016-03-25 21:56:01 -0500

    Adding mersenne-twister.

---
+++ mersenne-twister.d.ts
@@ -0,0 +1,12 @@
+export declare class MersenneTwister {
+	constructor(seed: number = null);
+
+	public random(): number;
+	public random_int(): number;
+	public random_incl(): number;
+	public random_excl(): number;
+	public random_long(): number;
+	public random_int31(): number;
+}
+
+export = MersenneTwister;
